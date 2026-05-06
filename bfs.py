e8c139d9d98e35fd0e8b3d4b3bd93f0b62e22749
Mohamed Kirate <mkirate@e1r5p8.1337.ma>
2026-05-06 10:58:40 +0100

    graph

---
+++ bfs.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+class BFS:
+    def __init__(self, graph, start):
+        self.__graph = graph
+        self.__start = start
+        self.__queue = deque([self.__start])
+        self.__visted = set()
+        self.__prev_setps = {node: None for node in self.__graph}
+
+    def start(self):
+        while self.__queue:
+            current = self.__queue.popleft()
+            for neighbor in self.__graph.get(current, []):
+                if neighbor not in self.__visted:
+                    self.__visted.add(neighbor)
+                    self.__prev_setps[neighbor] = current
+                    self.__queue.append(neighbor)
+
+    def get_path(self, target):
+        path = []
+        while target:
+            path.append(target)
+            target = self.__prev_setps.get(target)
+        return path[::-1]
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': [],
+    'F': []
+}
+
+bfs = BFS(graph, 'A')
+bfs.start()
+path = bfs.get_path('F')
+print(path)
---
+++ dfs.py
@@ -0,0 +1,34 @@
+class DFS:
+    def __init__(self, graph, start):
+        self.__start = start
+        self.__graph = graph
+        self.__stack = [self.__start]
+        self.__prev_steps = {node: None for node in self.__graph}
+    
+    def start(self):
+        while self.__stack:
+            current = self.__stack.pop()
+            for neighbor in self.__graph.get(current, []):
+                self.__prev_steps[neighbor] = current
+                self.__stack.append(neighbor)
+
+    def get_path(self, target):
+        path = []
+        while target:
+            path.append(target)
+            target = self.__prev_steps.get(target)
+        return path[::-1]
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': [],
+    'F': []
+}
+
+dfs = DFS(graph, 'A')
+dfs.start()
+path = dfs.get_path('F')
+print(path)
